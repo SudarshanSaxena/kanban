1b3fe6cd0edef833c9282f301acb2f1812fcbd71
Sudarshan Saxena <sudarshan.github@gmail.com>
2025-02-15 02:41:39 +0530

    FE done, BE init

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
 import {
   CdkDragDrop,
   CdkDrag,
@@ -7,20 +7,36 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+import { Board } from 'src/models/board.model';
+import { Column } from 'src/models/column.model';
 
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
-  styleUrls: ['./main-view.component.scss']
+  styleUrls: ['./main-view.component.scss'],
+  standalone: false
 })
 export class MainViewComponent {
-  isNavbarCollapsed = true;
+  @ViewChildren(CdkDropList) dropListRefs!: QueryList<CdkDropList>;
+  dropLists: CdkDropList[] = [];
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  ngAfterViewInit() {
+    // Collect all drop lists after view initialization
+    this.dropLists = this.dropListRefs.toArray();
+  }
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  board: Board = new Board('Test Board', [
+    new Column('IDEAS', ['one', 'two', 'three']),
+    new Column('TODO', ['one', 'two', 'three']),
+    new Column('IN Progress', ['one', 'two', 'three']),
+    new Column('REVIEW', ['one', 'two', 'three']),
+    new Column('DONE', ['one', 'two', 'three'])
+  ])
+
 
   drop(event: CdkDragDrop<string[]>) {
+    console.log(event)
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
